537fdc6d378c523b0632e2ec7e2760ba47063828
miyagiz <yagiz201551@gmail.com>
2022-09-21 16:16:03 +0300

    Completing the login process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ToastrModule.forRoot({
+      progressBar:true,
+      closeButton:true,
+      timeOut:3000
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { LoginModel } from 'src/app/models/loginModel';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isLoginButtonActive: boolean = true;
+  eMail: string = '';
+  password: string = '';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    //Sayfa yüklendiği an çalışır.
+    this.createLoginForm();
+    // console.log(this.isLoginButtonActive) Değeri Bu şekilde Kontrol edilebiliyor.
+  }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.isLoginButtonActive = false;
+      let loginModel = Object.assign({}, this.loginForm.value);
+      this.authService.login(loginModel).subscribe(
+        (res) => {
+          //console.log(res);
+          if (this.authService.redirectUrl) {
+            this.router.navigate([this.authService.redirectUrl]);
+          } else {
+            this.router.navigate(['']);
+          }
+          this.toastr.success(res.message, 'BAŞARILI');
+          localStorage.setItem('token', res.data.token);
+        },
+        (err) => {
+          this.isLoginButtonActive = true;
+          this.toastr.error(err.error, 'HATA!');
+        }
+      );
+    } else {
+      this.toastr.error('Eksik Bilgileri Doldurunuz!', 'HATA!');
+    }
+  }
+
+  changeClassEmailInput(text: string) {
+    if (text.includes('@')) {
+      return 'input-group input-group-outline is-valid my-3';
+    } else {
+      return 'input-group input-group-outline is-invalid my-3';
+    }
+  }
+
+  changeClassPasswordInput(text: string) {
+    if (text != '') {
+      return 'input-group input-group-outline is-valid my-3';
+    } else {
+      return 'input-group input-group-outline is-invalid my-3';
+    }
+  }
+}
